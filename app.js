8ef519ad1327807f028ef853059dcdc3079d8e81
jorodrig96 <jo.rodrig96@gmail.com>
2024-05-04 19:11:46 -0700

    created a class that has attributes and some default values. created a method in that class that lets you know if you or the player have won

--- app.js
+++ app.js
@@ -1,7 +1,31 @@
 class Casino {
-    // Write code here
-};
-
+    constructor(name, fakeCoin) {
+      this.name = name;
+      this.timesPlayed = 0;
+      this.winnings = 0;
+      this.fakeCoin = fakeCoin;
+    }
+  
+    playGame(betAmount) {
+      this.timesPlayed += 1;
+      if (Math.random() > 0.5 && this.fakeCoin == false) {
+        console.log("You won " + betAmount *
+          (this.timesPlayed) + " dollars from "
+          + this.name + "!");
+        this.winnings += this.timesPlayed * betAmount;
+        console.log("Current winnings: " + this.winnings);
+      } else {
+        console.log(this.name + " wins!");
+        this.timesPlayed = 0;
+        this.winnings = 0;
+      }
+    }
+  
+    rollDice(sides) {
+      console.log(Math.floor(Math.random() * sides) + 1);
+    }
+  }
+  
 // TESTS
 const myCasino = new Casino("HackerU Casino");
 console.log(myCasino);
